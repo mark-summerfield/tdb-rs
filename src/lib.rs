f9962fb803f0bf98cf8e0a4897fbde21bce26d16
mark-summerfield <mark@qtrac.eu>
2022-11-29 10:00:42 +0000

    started

---
+++ src/lib.rs
@@ -0,0 +1,17 @@
+// Copyright © 2022 Mark Summerfield. All rights reserved.
+// License: GPLv3
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn it_works() {
+        let result = add(2, 2);
+        assert_eq!(result, 4);
+    }
+}
---
+++ st.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+clc -sS
+grep --color=auto --exclude-dir=target --include=*.rs -r format......,
+git st
